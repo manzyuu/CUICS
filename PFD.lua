be31d6792a4443864f991f97d03fe2bbbd7268e4
manzyuu <119768658+manzyuu@users.noreply.github.com>
2023-06-08 16:25:40 +0900

    ちょっと書き直し

--- PFD.lua
+++ PFD.lua
@@ -50,31 +50,33 @@ end
 -- Tick function that will be executed every logic tick
 end
 
-m = math
+--m = math
 --min,max,abs,cos,sin,tan,acos,asin,atan,pi,(math.pi * 2) = m.min,m.max,m.abs,m.cos,m.sin,m.tan,m.acos,m.asin,m.atan,m.pi,m.pi*2
-function compassBar(targetangle)    --targetangle = degrees
-    local compassBarSpase = 300
 
-    screen.setColor(5,5,5)
-    screen.drawRectF(10,23,11,7)
-    screen.drawRectF(9,24,13,5)
 
-    screen.setColor(255, 255, 225)
-    --print(tonumber(math.floor(math.deg(Phys.compass_y))))
-    drawNewFont(10, 24, string.format("%03d", tonumber(math.floor(math.deg(Phys.compass_y)))))
+--定義部---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Phys = {}
 
-    screen.setColor(5, 70, 5)
-    for i = 0, 72, 1 do
-        screen.drawLine(math.floor(((math.floor(math.deg(Phys.compass_y)-i*5+360)%360)/360)*compassBarSpase), 29+i%2,
-                        math.floor(((math.floor(math.deg(Phys.compass_y)-i*5+360)%360)/360)*compassBarSpase), 33)
-    end
-    
-    screen.setColor(250, 250, 40)
-    screen.drawLine(math.floor(((math.floor(math.deg(Phys.compass_y)-targetangle+360)%360)/360)*compassBarSpase), 29,
-                    math.floor(((math.floor(math.deg(Phys.compass_y)-targetangle+360)%360)/360)*compassBarSpase), 33)
-end
+SkyColorR,SkyColorG,SkyColorB = Colorconv16("SkyColor")
+LandColorR,SkyColorG,SkyColorB = Colorconv16("LandColor")
+SkyColorR,SkyColorG,SkyColorB = Colorconv16("CenterLineColor")
+
+indicatorColor = property.getText("indicatorColor")
+--indicatorColor = "200,100,100,100,200,100,100,100,200,255,255,255"
+
+altunit = property.getNumber("Altitude Unit")
+spdunit = property.getNumber("Speed Unit")
+
+firstTick = true
+monitorPower = true
+
+indicatorbool = {false, false, false, false}
+
+
+
+--数学系
 function convert()
-    Phys = {}
+    
     -- This uses the game's basis. That is,
     --     vehicle   world   color
     -- x   right     east    red
@@ -82,128 +84,66 @@ function convert()
     -- z   forward   north   blue
 
     -- Get angles.
-    local x = input.getNumber(4)
-    local y = input.getNumber(5)
-    local z = input.getNumber(6)
+    Phys.x     = input.getNumber(1)
+    Phys.alt   = input.getNumber(2)
+    Phys.y     = input.getNumber(3)
+    
+
+    local phyX = input.getNumber(4)
+    local phyY = input.getNumber(5)
+    local phyZ = input.getNumber(6)
+    
+    -- Get global angular velocities.
+    local phyAngX = input.getNumber(10)
+    local phyAngY = input.getNumber(11)
+    local phyAngZ = input.getNumber(12)
 
     -- sin, 'cos I like it.
-    local cx, sx = math.cos(x), math.sin(x)
-    local cy, sy = math.cos(y), math.sin(y)
-    local cz, sz = math.cos(z), math.sin(z)
+    local cx, sx = math.cos(phyX), math.sin(phyX)
+    local cy, sy = math.cos(phyY), math.sin(phyY)
+    local cz, sz = math.cos(phyZ), math.sin(phyZ)
 
     -- Build matrix.
-    local m00 =  cy*cz
-    local m01 = -cx*sz + sx*sy*cz
-    local m02 =  sx*sz + cx*sy*cz
-    local m10 =  cy*sz
-    local m11 =  cx*cz + sx*sy*sz
-    local m12 = -sx*cz + cx*sy*sz
-    local m20 = -sy
-    local m21 =  sx*cy
-    local m22 =  cx*cy
-    --[[
-    -- Compute tilts. Less stable near gimbal lock. m11 and m12 can exceed asin's domain.
-    Phys.tilt_x = math.asin(m10) / math.pi * 2
-    Phys.tilt_y = math.asin(m11) / math.pi * 2
-    Phys.tilt_z = math.asin(m12) / math.pi * 2
-
-    -- Alternatively, compute tilts stably.
-    Phys.tilt_x = math.atan(m10, math.sqrt(m00*m00 + m20*m20)) / math.pi * 2
-    Phys.tilt_y = math.atan(m11, math.sqrt(m01*m01 + m21*m21)) / math.pi * 2
-    Phys.tilt_z = math.atan(m12, math.sqrt(m02*m02 + m22*m22)) / math.pi * 2
-    ]]
+    local matrix00 =  cy*cz
+    local matrix01 = -cx*sz + sx*sy*cz
+    local matrix02 =  sx*sz + cx*sy*cz
+    local matrix10 =  cy*sz
+    local matrix11 =  cx*cz + sx*sy*sz
+    local matrix12 = -sx*cz + cx*sy*sz
+    local matrix20 = -sy
+    local matrix21 =  sx*cy
+    local matrix22 =  cx*cy
     -- Equally good alternative if you prefer.
-    Phys.tilt_x = math.atan(m10, math.sqrt(m12*m12 + m11*m11)) / (math.pi * 2)
-    Phys.tilt_y = math.atan(m11, math.sqrt(m10*m10 + m12*m12)) / (math.pi * 2)
-    Phys.tilt_z = math.atan(m12, math.sqrt(m11*m11 + m10*m10)) / (math.pi * 2)
+    Phys.tilt_x = math.atan(matrix10, math.sqrt(matrix12*matrix12 + matrix11*matrix11)) / (math.pi * 2)
+    Phys.tilt_y = math.atan(matrix11, math.sqrt(matrix10*matrix10 + matrix12*matrix12)) / (math.pi * 2)
+    Phys.tilt_z = math.atan(matrix12, math.sqrt(matrix11*matrix11 + matrix10*matrix10)) / (math.pi * 2)
 
     -- Compute compasses.
-    Phys.compass_x = math.atan(m00, m20) / -(math.pi * 2)
-    --Phys.compass_y = math.atan(m01, m21) / -(math.pi * 2)
-    Phys.compass_z = math.atan(m02, m22) / -(math.pi * 2)
+    Phys.compass_x = math.atan(matrix00, matrix20) / -(math.pi * 2)
+    Phys.compass_y = math.atan(matrix01, matrix21) / -(math.pi * 2)
+    Phys.compass_z = math.atan(matrix02, matrix22) / -(math.pi * 2)
 
-    local compassSensor = -math.atan(math.sin(x) * math.sin(z) + math.cos(x) * math.sin(y) * math.cos(z),
-    math.cos(x) * math.cos(y)) / 2 / math.pi
-    temp = (((1 - compassSensor) % 1) * (math.pi * 2))
+    --local compassSensor = -math.atan(sx * sz + cx * sy * cz,cx * cy) / 2 / math.pi
+    temp = (((1 - Phys.compass_y) % 1) * (math.pi * 2))
     Phys.compass_y = isInteger(temp)
     --Phys.compass_y = type(temp) == "number" and temp or 0
 
-    -- Get global angular velocities.
-    local ax = input.getNumber(10)
-    local ay = input.getNumber(11)
-    local az = input.getNumber(12)
+
 
     -- Transform them to the local frame.
-    Phys.angular_x = m00*ax + m10*ay + m20*az
-    Phys.angular_y = m01*ax + m11*ay + m21*az
-    Phys.angular_z = m02*ax + m12*ay + m22*az
+    Phys.angular_x = matrix00*phyAngX + matrix10*phyAngY + matrix20*phyAngZ
+    Phys.angular_y = matrix01*phyAngX + matrix11*phyAngY + matrix21*phyAngZ
+    Phys.angular_z = matrix02*phyAngX + matrix12*phyAngY + matrix22*phyAngZ
 
-    Phys.x     = input.getNumber(1)
-    Phys.y     = input.getNumber(3)
-    Phys.alt   = input.getNumber(2)
+
     --Phys.compass_y   = Phys.compass_y
-    ----@diagnostic disable-next-line: redundant-parameter
     Phys.roll  = math.atan(Phys.tilt_y, Phys.tilt_x)
     --Phys.roll = 0
     Phys.pitch = Phys.tilt_z
 end
-function drawNewFont(NewFontX,NewFontY,NewFontZ)
-	local NewFontC,NewFontD,NewFontF,NewFontP,NewFontQ
-	if type(NewFontZ)=="number"then
-		NewFontZ=tostring(NewFontZ)
-	end
-	NewFontD=property.getText("F1")..property.getText("F2")..property.getText("F3")..property.getText("F4")
-	for i=1,NewFontZ:len()do
-		NewFontC=NewFontZ:sub(i,i):byte()*5-159
-		for j=1,5 do
-			NewFontF="0x"..NewFontD:sub(NewFontC,NewFontC+4):sub(j,j)
-				for k=1,3 do
-				if NewFontF&2^(4-k)>0 then
-					NewFontP=NewFontX+i*4+k-5
-					NewFontQ=NewFontY+j-1
-					screen.drawLine(NewFontP,NewFontQ,NewFontP+1,NewFontQ)
-				end
-			end
-		end
-	end
-end
-function Indicator(boolA,boolB,boolC,boolD) --四角4つ
-    colorTable=split(indicatorColor,",")
-    if boolA then
-        screen.setColor(tonumber(colorTable[1]),tonumber(colorTable[2]),tonumber(colorTable[3]))
-    else
-        screen.setColor(10,10,10)
-    end
-    screen.drawRectF(0,4,4,3)--
-    
-    
-    
-    if boolB then
-        screen.setColor(tonumber(colorTable[4]),tonumber(colorTable[5]),tonumber(colorTable[6]))
-    else
-        screen.setColor(10,10,10)
-    end
-    screen.drawRectF(0,10,4,3)--
-    
-    
-    
-    if boolC then
-        screen.setColor(tonumber(colorTable[7]),tonumber(colorTable[8]),tonumber(colorTable[9]))
-    else
-        screen.setColor(10,10,10)
-    end
-    screen.drawRectF(0,16,4,3)--
 
+--処理系---------------------------------------------------
 
-
-
-    if boolD then
-        screen.setColor(tonumber(colorTable[7]),tonumber(colorTable[8]),tonumber(colorTable[9]))
-    else
-        screen.setColor(10,10,10)
-    end
-    screen.drawRectF(0,22,4,3)--
-end
 function split(str, delim)
     if string.find(str, delim) == nil then
         return { str }
@@ -239,6 +179,31 @@ function isInteger(inVal)
         return 0
     end
 end
+
+--Font---------------------------------------------------
+
+function drawNewFont(NewFontX,NewFontY,NewFontZ)
+	local NewFontC,NewFontD,NewFontF,NewFontP,NewFontQ
+	if type(NewFontZ)=="number"then
+		NewFontZ=tostring(NewFontZ)
+	end
+	NewFontD=property.getText("F1")..property.getText("F2")..property.getText("F3")..property.getText("F4")
+	for i=1,NewFontZ:len()do
+		NewFontC=NewFontZ:sub(i,i):byte()*5-159
+		for j=1,5 do
+			NewFontF="0x"..NewFontD:sub(NewFontC,NewFontC+4):sub(j,j)
+				for k=1,3 do
+				if NewFontF&2^(4-k)>0 then
+					NewFontP=NewFontX+i*4+k-5
+					NewFontQ=NewFontY+j-1
+					screen.drawLine(NewFontP,NewFontQ,NewFontP+1,NewFontQ)
+				end
+			end
+		end
+	end
+end
+--描画系---------------------------------------------------
+
 function horizon()  --水平儀
     local roll  = Phys.roll * (math.pi * 2)
     local pitch = Phys.pitch * (math.pi * 2)
@@ -279,6 +244,64 @@ function horizon()  --水平儀
     screen.setColor(255,255,255)
     screen.drawText(16,25,pitch)
 end
+function compassBar(targetangle)    --targetangle = degrees
+    local compassBarSpase = 300
+
+    screen.setColor(5,5,5)
+    screen.drawRectF(10,23,11,7)
+    screen.drawRectF(9,24,13,5)
+
+    screen.setColor(255, 255, 225)
+    --print(tonumber(math.floor(math.deg(Phys.compass_y))))
+    drawNewFont(10, 24, string.format("%03d", tonumber(math.floor(math.deg(Phys.compass_y)))))
+
+    screen.setColor(5, 70, 5)
+    for i = 0, 72, 1 do
+        screen.drawLine(math.floor(((math.floor(math.deg(Phys.compass_y)-i*5+360)%360)/360)*compassBarSpase), 29+i%2,
+                        math.floor(((math.floor(math.deg(Phys.compass_y)-i*5+360)%360)/360)*compassBarSpase), 33)
+    end
+    
+    screen.setColor(250, 250, 40)
+    screen.drawLine(math.floor(((math.floor(math.deg(Phys.compass_y)-targetangle+360)%360)/360)*compassBarSpase), 29,
+                    math.floor(((math.floor(math.deg(Phys.compass_y)-targetangle+360)%360)/360)*compassBarSpase), 33)
+end
+function Indicator(boolA,boolB,boolC,boolD) --四角4つ
+    colorTable=split(indicatorColor,",")
+    if boolA then
+        screen.setColor(tonumber(colorTable[1]),tonumber(colorTable[2]),tonumber(colorTable[3]))
+    else
+        screen.setColor(10,10,10)
+    end
+    screen.drawRectF(0,4,4,3)--
+    
+    
+    
+    if boolB then
+        screen.setColor(tonumber(colorTable[4]),tonumber(colorTable[5]),tonumber(colorTable[6]))
+    else
+        screen.setColor(10,10,10)
+    end
+    screen.drawRectF(0,10,4,3)--
+    
+    
+    
+    if boolC then
+        screen.setColor(tonumber(colorTable[7]),tonumber(colorTable[8]),tonumber(colorTable[9]))
+    else
+        screen.setColor(10,10,10)
+    end
+    screen.drawRectF(0,16,4,3)--
+
+
+
+
+    if boolD then
+        screen.setColor(tonumber(colorTable[7]),tonumber(colorTable[8]),tonumber(colorTable[9]))
+    else
+        screen.setColor(10,10,10)
+    end
+    screen.drawRectF(0,22,4,3)--
+end
 function altimeter()
     screen.setColor(255,10,50,100)  --背景
     screen.drawRectF(0,12,32,7)
@@ -297,20 +320,12 @@ function altimeter()
     screen.drawTextBox(6, 0, 26, 32, string.format("%04d", math.floor(Phys.alt * altunit)), 0, 0)
 end
 
-SkyColorR,SkyColorG,SkyColorB = Colorconv16("SkyColor")
-LandColorR,SkyColorG,SkyColorB = Colorconv16("LandColor")
-SkyColorR,SkyColorG,SkyColorB = Colorconv16("CenterLineColor")
 
-indicatorColor = property.getText("indicatorColor")
---indicatorColor = "200,100,100,100,200,100,100,100,200,255,255,255"
 
-altunit = property.getNumber("Altitude Unit")
-spdunit = property.getNumber("Speed Unit")
 
-firstTick = true
-monitorPower = true
 
-indicatorbool = {false, false, false, false}
+--------------実行部------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 function onTick()					--[====[ onTick ]====]--
     if firstTick then
